5046231ed5f6c3d5a572d8b726ee1af8d0a9cd28
Bart Feenstra <bart@mynameisbart.com>
2015-01-26 12:30:37 +0100

    Fixed #16014: content_provider data type.

---
+++ src/ContentProvider.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @file
+ * Contains \Triquanta\IziTravel\ContentProviderInterface.
+ */
+
+namespace Triquanta\IziTravel;
+
+/**
+ * Provides a content provider data type.
+ */
+class ContentProvider implements ContentProviderInterface {
+
+  use UuidTrait;
+
+  /**
+   * The copyright message.
+   *
+   * @var string|null
+   */
+  protected $copyrightMessage;
+
+  /**
+   * The name.
+   *
+   * @var string
+   */
+  protected $name;
+
+  /**
+   * Creates a new instance.
+   *
+   * @param string $uuid
+   * @param string $name
+   * @param string $copyright_message
+   */
+  public function __construct($uuid, $name, $copyright_message) {
+    $this->uuid = $uuid;
+    $this->name = $name;
+    $this->copyrightMessage = $copyright_message;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createFromJson($json) {
+    $data = json_decode($json);
+    if (is_null($data)) {
+      throw new InvalidJsonFactoryException($json);
+    }
+    $data = (array) $data + [
+        'copyright' => NULL,
+      ];
+    if (!isset($data['uuid'])) {
+      throw new MissingUuidFactoryException($json);
+    }
+    return new static($data['uuid'], $data['name'], $data['copyright']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName() {
+    return $this->name;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCopyrightMessage() {
+    return $this->copyrightMessage;
+  }
+
+}
---
+++ tests/ContentProviderTest.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * @file
+ * Contains \Triquanta\IziTravel\Tests\ContentProviderTest.
+ */
+
+namespace Triquanta\IziTravel\Tests;
+
+use Triquanta\IziTravel\ContentProvider;
+
+/**
+ * @coversDefaultClass \Triquanta\IziTravel\ContentProvider
+ */
+class ContentProviderTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * The copyright message.
+   *
+   * @var string
+   */
+  protected $copyrightMessage;
+
+  /**
+   * The name.
+   *
+   * @var string
+   */
+  protected $name;
+
+  /**
+   * The UUID.
+   *
+   * @var string
+   */
+  protected $uuid;
+
+  /**
+   * The class under test.
+   *
+   * @var \Triquanta\IziTravel\ContentProvider
+   */
+  protected $sut;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    $this->uuid = 'foo-bar-' . mt_rand();
+    $this->name = 'Foo Baz';
+    $this->copyrightMessage = 'Private property!';
+
+    $this->sut = new ContentProvider($this->uuid, $this->name, $this->copyrightMessage);
+  }
+
+  /**
+   * @covers ::__construct
+   * @covers ::createFromJson
+   */
+  public function testCreateFromJson() {
+    $json = <<<'JSON'
+{
+  "name": "Sample CP",
+  "uuid": "15ad4ee2-ff55-4a86-950d-8dee4c79fc35"
+}
+JSON;
+
+    ContentProvider::createFromJson($json);
+  }
+
+  /**
+   * @covers ::__construct
+   * @covers ::createFromJson
+   *
+   * @expectedException \Triquanta\IziTravel\InvalidJsonFactoryException
+   */
+  public function testCreateFromJsonWithInvalidJson() {
+    $json = 'foo';
+
+    ContentProvider::createFromJson($json);
+  }
+
+  /**
+   * @covers ::createFromJson
+   *
+   * @expectedException \Triquanta\IziTravel\MissingUuidFactoryException
+   */
+  public function testCreateFromJsonWithoutUuid() {
+    $json = <<<'JSON'
+{
+  "name": "Sample CP"
+}
+JSON;
+
+    ContentProvider::createFromJson($json);
+  }
+
+  /**
+   * @covers ::getName
+   */
+  public function testGetName() {
+    $this->assertSame($this->name, $this->sut->getName());
+  }
+
+  /**
+   * @covers ::getCopyrightMessage
+   */
+  public function testGetCopyrightMessage() {
+    $this->assertSame($this->copyrightMessage, $this->sut->getCopyrightMessage());
+  }
+
+}
