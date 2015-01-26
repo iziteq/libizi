849421bb296c4108b1ff788cc1bab6387f70c860
Bart Feenstra <bart@mynameisbart.com>
2015-01-26 14:52:47 +0100

    Fixed #16015: playback data type.

---
+++ src/PlaybackInterface.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Contains \Triquanta\IziTravel\PlaybackInterface.
+ */
+
+namespace Triquanta\IziTravel;
+
+/**
+ * Defines a playback data type.
+ */
+interface PlaybackInterface extends FactoryInterface {
+
+  /**
+   * Sequential playback.
+   */
+  const TYPE_SEQUENTIAL = 'sequential';
+
+  /**
+   * Random playback.
+   */
+  const TYPE_RANDOM = 'random';
+
+  /**
+   * Gets the type.
+   *
+   * @return string
+   *   One of the static::TYPE_* constants.
+   */
+  public function getType();
+
+  /**
+   * Gets whether playback is random.
+   *
+   * @return bool
+   */
+  public function isRandom();
+
+  /**
+   * Gets whether playback is sequential.
+   *
+   * @return bool
+   */
+  public function isSequential();
+
+  /**
+   * Gets the UUIDs.
+   *
+   * @return string[]
+   */
+  public function getUuids();
+
+}
---
+++ tests/PlaybackTest.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * @file
+ * Contains \Triquanta\IziTravel\Tests\PlaybackTest.
+ */
+
+namespace Triquanta\IziTravel\Tests;
+
+use Triquanta\IziTravel\Playback;
+
+/**
+ * @coversDefaultClass \Triquanta\IziTravel\Playback
+ */
+class PlaybackTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * The type.
+   *
+   * @var string
+   *   One of the static::TYPE_* constants.
+   */
+  protected $type;
+
+  /**
+   * The UUIDs.
+   *
+   * @var string[]
+   */
+  protected $uuids = [];
+
+  /**
+   * The class under test.
+   *
+   * @var \Triquanta\IziTravel\Playback
+   */
+  protected $sut;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    $this->type = Playback::TYPE_RANDOM;
+    $this->uuids = ['foo-bar-baz-' . mt_rand(), 'foo-bar-qux-' . mt_rand()];
+
+    $this->sut = new Playback($this->type, $this->uuids);
+  }
+
+  /**
+   * @covers ::__construct
+   * @covers ::createFromJson
+   */
+  public function testCreateFromJson() {
+    $json = <<<'JSON'
+{
+  "type": "sequential",
+  "order": [
+    "3afcd4ab-837f-4055-a8ed-ce43910f9446",
+    "7b5092de-43f3-4762-9142-df30529f7942"
+  ]
+}
+JSON;
+
+    Playback::createFromJson($json);
+  }
+
+  /**
+   * @covers ::__construct
+   * @covers ::createFromJson
+   *
+   * @expectedException \Triquanta\IziTravel\InvalidJsonFactoryException
+   */
+  public function testCreateFromJsonWithInvalidJson() {
+    $json = 'foo';
+
+    Playback::createFromJson($json);
+  }
+
+  /**
+   * @covers ::getType
+   */
+  public function testGetType() {
+    $this->assertSame($this->type, $this->sut->getType());
+  }
+
+  /**
+   * @covers ::getUuids
+   */
+  public function testGetUuids() {
+    $this->assertSame($this->uuids, $this->sut->getUuids());
+  }
+
+  /**
+   * @covers ::isRandom
+   */
+  public function testIsRandom() {
+    $playback_random = new Playback(Playback::TYPE_RANDOM, $this->uuids);
+    $playback_sequantial = new Playback(Playback::TYPE_SEQUENTIAL, $this->uuids);
+
+    $this->assertTrue($playback_random->isRandom());
+    $this->assertFalse($playback_sequantial->isRandom());
+  }
+
+  /**
+   * @covers ::isSequential
+   */
+  public function testIsSequential() {
+    $playback_random = new Playback(Playback::TYPE_RANDOM, $this->uuids);
+    $playback_sequantial = new Playback(Playback::TYPE_SEQUENTIAL, $this->uuids);
+
+    $this->assertFalse($playback_random->isSequential());
+    $this->assertTrue($playback_sequantial->isSequential());
+  }
+
+}
